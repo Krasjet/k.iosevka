478cf6cac41294f8c2798045a2632e948ba3f249
krasjet <nil@krj.st>
2020-06-29 15:42:34 -0700

    add nerd font patcher

--- config.mk
+++ config.mk
@@ -1,3 +1,8 @@
 # Iosevka version
-VERSION = 3.2.2
-ARCHIVE_URL = https://github.com/be5invis/Iosevka/archive/v$(VERSION).tar.gz
+IOSEVKA_VERSION = 3.2.2
+IOSEVKA_URL = https://github.com/be5invis/Iosevka/archive/v$(IOSEVKA_VERSION).tar.gz
+
+# Nerd font version
+NERDFONT_VERSION = 2.1.0
+NERDFONT_SVN = https://github.com/ryanoasis/nerd-fonts/tags/v$(NERDFONT_VERSION)
+NERDFONT_GIT = https://raw.githubusercontent.com/ryanoasis/nerd-fonts/v$(NERDFONT_VERSION)
